06f852fc26c0dfa132f48554c8c2d76bcb6d4ece
duda <duda@parmz.parmz>
2025-09-24 22:41:49 +0200

    faster nav loading

--- nav.js
+++ nav.js
@@ -1,24 +1,4 @@
-function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                observer.disconnect();
-                resolve(document.querySelector(selector));
-            }
-        });
-
-        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-}
-
 const nav_promise = fetch("./nav.html").then((f) => {return f.text()})
-const nav_elem = await waitForElm("#nav")
+const nav_elem = document.getElementById("nav")
 nav_promise.then((v) => nav_elem.innerHTML = v)
+window.matchMedia()
