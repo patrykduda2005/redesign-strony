706a53511e64d123b332579bf8364d1eab92259e
duda <duda@parmz.parmz>
2025-10-16 20:00:00 +0200

    chyba gites

--- nav.js
+++
@@ -1,4 +0,0 @@
-const nav_promise = fetch("./nav.html").then((f) => {return f.text()})
-const nav_elem = document.getElementById("nav")
-nav_promise.then((v) => nav_elem.innerHTML = v)
-window.matchMedia()
